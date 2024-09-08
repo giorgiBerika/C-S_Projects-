8c41d4033773f9a66c57956c8419131cc9979493
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2024-09-08 17:06:00 +0400

    Adding new record system/Saving data to Database

--- student-managements/main.cpp
+++ student-managements/main.cpp
@@ -6,8 +6,77 @@
 #include <cppconn/resultset.h>
 #include <cppconn/exception.h>
 
+#include <string>
+
+// Constants
+
+const std::string tabSpace(20, ' ');
+const std::string dashBar(29, '-');
+const std::string fullDashLine = tabSpace + dashBar;
+
+// Print Menu functions ///
+void printMenuOption(int optionNumber, const std::string &description)
+{
+    std::cout << tabSpace << optionNumber << ". " << description << std::endl;
+};
+
+void menuInfo()
+{
+
+    std::cout<<fullDashLine<<std::endl;
+    std::cout<<tabSpace<<"| STUDENT MANAGEMENT SYSTEM |"<<std::endl;
+    std::cout<<fullDashLine<<"\n"<<std::endl;
+
+    printMenuOption(1, "Enter New Record");
+    printMenuOption(2, "Search Record");
+    printMenuOption(3, "Modify Record");
+    printMenuOption(4, "Delete Record");
+    printMenuOption(5, "Exit");
+      
+    std::cout<<"\n";
+    std::cout<<fullDashLine<<std::endl;
+    std::cout<<tabSpace<<"Choose Option: [1/2/3/4/5]"<<std::endl;
+    std::cout<<fullDashLine<<std::endl;
+};
+
+class Student
+{
+    public:
+        std::string name;
+        int age;
+        std::string major;
+
+        void inputStudentInfo()
+        {
+            std::cout << "Enter student Name: ";
+            // std::cin >> name;
+            getline(std::cin, name);
+            std::cout << "Enter student Age: ";
+            std::cin >> age;
+            std::cout << "Enter student Major: ";
+            // std::cin >> major;
+            getline(std::cin, major);
+        }
+
+
+        void saveToDatabase (std::unique_ptr<sql::Statement>& stmt)
+        {
+            std::string query = "INSERT INTO students (name, age, major) VALUES ('" + 
+                        name +"', '" + std::to_string(age) + "', '"+ major + "')";
+
+            stmt->execute(query);
+            std::cout << "Student data saved to database." << std::endl;
+        }    
+        
+
+};
+
 int main() {
+
+    menuInfo();
+
     try {
+        // ---------Connecting to Databse ---------------
         // Create a smart pointer for the driver and connection
         sql::mysql::MySQL_Driver *driver;
         std::unique_ptr<sql::Connection> con;
@@ -24,22 +93,23 @@ int main() {
         // Create a statement object
         std::unique_ptr<sql::Statement> stmt(con->createStatement());
         
-        // My Changes ///
-
-        stmt->execute("INSERT INTO student (name, email) VALUES ('Gigi Mamulashvili', 'gmamula@gmail.com')");
-        // 
+    //    -----------------------------------------------
+        Student student;
+        student.inputStudentInfo();
+        student.saveToDatabase(stmt);
 
         // Execute a query and retrieve the result
-        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT * FROM student"));
+        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT * FROM students"));
 
         // Process the result set
         while (res->next()) {
-            std::cout << "Name: " << res->getString("name");
-            std::cout << " Email: " << res->getString("email") << std::endl;
+            std::cout << "Name: " << res->getString("name") << "\n";
+            std::cout << "Age: " << res->getString("age") << "\n"; 
+            std::cout << "Major: " << res->getString("major") << std::endl;
         }
 
     } catch (sql::SQLException &e) {
-        // std::cerr << "SQLException: " << e.what() << std::endl;
+        std::cerr << "SQLException: " << e.what() << std::endl;
         return 1;
     }
 
